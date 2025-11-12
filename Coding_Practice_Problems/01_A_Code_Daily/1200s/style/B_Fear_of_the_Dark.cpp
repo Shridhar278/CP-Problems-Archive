d7ae7b801239993133169cb65cb998d2a5fde9ef
Shridhar Sharma <sridharsharma278@gmail.com>
2025-11-12 18:34:08 +0530

    a

--- Coding_Practice_Problems/01_A_Code_Daily/1200s/style/B_Fear_of_the_Dark.cpp
+++ Coding_Practice_Problems/01_A_Code_Daily/1200s/style/B_Fear_of_the_Dark.cpp
@@ -1,41 +1,11 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define int long long
-#define INF (int)1e9+7
-
-inline void fast_io() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-}
-
-double distance(int x1, int y1, int x2, int y2) {
-    return sqrt(pow((x1-x2), 2) + pow((y1-y2), 2));
-}
-
-int32_t main() {
-    fast_io();
-    int test;
-    cin >> test;
-    while (test--) {
-        int px, py, ax, ay, bx, by;
-        cin >> px >> py >> ax >> ay >> bx >> by;
-        double ab = distance(ax, ay, bx, by);
-        double ap = distance(ax, ay, px, py);
-        double bp = distance(px, py, bx, by);
-        double porg = distance(px, py, 0, 0);
-        double aorg = distance(ax, ay, 0, 0);
-        double borg = distance(bx, by, 0, 0);
-        double final_dist = 0;
-        bool intersect=true;
-        if ((min(ap, aorg) >= max(bp, borg))||(min(bp, borg) >= max(ap, aorg))) {
-            intersect=false;
-        }
-        if (intersect==false) {
-            final_dist = min(max(ap, aorg), max(bp, borg));
-        } else {
-            final_dist = max({ab/2, min(ap, bp), min(aorg, borg)});
-        }
-        cout<<fixed<<setprecision(10)<<final_dist<<endl;
-    }
-    return 0;
-}
+                                        #include <bits/stdc++.h> 
+inline void fast_io() { std::ios::sync_with_stdio(false); std::cin.tie(nullptr); } double distance(int/**/
+x1,int y1,int x2, int y2){return sqrt(pow((x1-x2), 2) + pow((y1-y2), 2));} int32_t main() { fast_io();/**/
+int test; std::cin >> test; while (test--) { int px, py, ax, ay, bx, by; std::cin >> px >> py >> ax >>/**/
+ay >> bx >> by; double ab = distance(ax, ay, bx, by); double ap = distance( ax, ay, px, py); double bp/**/
+= distance(px, py, bx, by); double porg = distance(px, py, 0, 0);double aorg = distance(ax, ay, 0, 0);/**/
+double borg = distance(bx, by, 0, 0) ; double final_dist = 0 ; bool intersect=true;if ((std::min(ap, aorg)
+>= std::max(bp, borg))||(std::min(bp, borg) >= std::max(ap, aorg))) { intersect = false ;} if (intersect==
+false){ final_dist = std::min( std::max(ap, aorg), std :: max(bp, borg)) ; } else { final_dist = std::max(
+{ab/2, std::min(ap, bp), std::min(aorg, borg)}) ;}std::cout<<std::fixed<<std::setprecision(10)<<final_dist
+<<std::endl;}return 0;}/***************************1886/B. Fear of the Dark******************************/
