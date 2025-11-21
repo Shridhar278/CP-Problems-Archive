5fce28e9ee41f7c1b52e33aa445ba82265fc10dd
Shridhar Sharma <sridharsharma278@gmail.com>
2025-11-21 16:29:47 +0530

    x

---
+++ Coding_Practice_Problems/00_The_Grind/B_Distinct_Elements.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define INF (int)1e9+7
+#define EPS (double)1e-9
+
+inline void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int32_t main() {
+    fast_io();
+    int test;
+    cin >> test;
+    while (test--) {
+        
+    }
+    return 0;
+}
