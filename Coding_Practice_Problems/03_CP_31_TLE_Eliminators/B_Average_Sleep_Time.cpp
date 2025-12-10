50e2a4842d8b979b61df3e164426c5339d5304ee
Shridhar Sharma <sridharsharma278@gmail.com>
2025-12-10 18:28:39 +0530

    X

--- Coding_Practice_Problems/03_CP_31_TLE_Eliminators/B_Average_Sleep_Time.cpp
+++ Coding_Practice_Problems/03_CP_31_TLE_Eliminators/B_Average_Sleep_Time.cpp
@@ -1,16 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-#define INF (int)1e9+7
+#define INF (int)1e9 + 7
 #define EPS (double)1e-9
 
-inline void fast_io() {
+inline void fast_io()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 }
 
-int32_t main() {
+int32_t main()
+{
     fast_io();
-    
+    int n;
+    for (int i = 0; i < n; i++)
+    {
+    }
     return 0;
 }
