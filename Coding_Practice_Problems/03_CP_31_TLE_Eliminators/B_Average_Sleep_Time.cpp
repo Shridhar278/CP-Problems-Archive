698cfc8f3d6a6e3019997d558fe309ab3334176d
Shridhar Sharma <sridharsharma278@gmail.com>
2025-12-10 18:21:13 +0530

    yes

---
+++ Coding_Practice_Problems/03_CP_31_TLE_Eliminators/B_Average_Sleep_Time.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define INF (int)1e9+7
+#define EPS (double)1e-9
+
+inline void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+int32_t main() {
+    fast_io();
+    
+    return 0;
+}
