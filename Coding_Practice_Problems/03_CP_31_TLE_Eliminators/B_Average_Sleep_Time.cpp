2e95bd8bade19c88b837acf1b3e0d14e6a6b5a1c
Shridhar Sharma <sridharsharma278@gmail.com>
2025-12-10 19:47:04 +0530

    1

--- Coding_Practice_Problems/03_CP_31_TLE_Eliminators/B_Average_Sleep_Time.cpp
+++ Coding_Practice_Problems/03_CP_31_TLE_Eliminators/B_Average_Sleep_Time.cpp
@@ -10,12 +10,33 @@ inline void fast_io()
     cin.tie(nullptr);
 }
 
+// use DOUBLE as 53 binary bits == 16 decimal bits for precision
+// this is more ACCURATE
+
+// FLOAT has only 23 binary bits == 7 decimal bits for precision
+// but since, its asked to CORRECT till 10^-6 precision FLOAT is ENOUGH
+// use double only for self satisfaction
+
 int32_t main()
 {
     fast_io();
-    int n;
-    for (int i = 0; i < n; i++)
-    {
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
+    int curr = 0, ttl = 0;
+    float num = n - k + 1;
+    for (int i = 0; i < k; i++) {
+        curr += arr[i];
+    }
+    ttl += curr;
+    for (int i = k; i < n; i++) {
+        curr += arr[i];
+        curr -= arr[i-k];
+        ttl += curr;
+    }
+    cout<<fixed<<setprecision(10)<<ttl/num<<endl;
     return 0;
 }
