05a2337e5a0c539b456f7554691f484980fa35cd
Shridhar Sharma <sridharsharma278@gmail.com>
2026-01-05 09:00:04 +0530

    103

--- 2026_CP_2026/C_Needle_in_a_Haystack.cpp
+++ 2026_CP_2026/C_Needle_in_a_Haystack.cpp
@@ -13,7 +13,40 @@ void solve() {
     string s, t;
     cin >> s;
     cin >> t;
-    
+    vector<int> alphaS(26, 0), alphaT(26, 0);
+    for (int i=0;i<s.size();i++) {
+        alphaS[s[i]-'a']++;
+    }
+    for (int i=0;i<t.size();i++) {
+        alphaT[t[i]-'a']++;    
+    }
+    for (int i=0;i<26;i++) {
+        if (alphaS[i] > alphaT[i]) {
+            cout<<"Impossible"<<endl;
+            return;
+        }
+    }
+    string res="";
+    int curr=0;
+    int order=0;
+    int phase = 0;
+    while(curr<26) {
+        if (not phase and s[order]<=(curr+'a')) {
+            res+=s[order];
+            order++;
+            if (order == s.size()) {
+                phase = 1;
+            }
+            continue;
+        }
+        if (alphaT[curr]>alphaS[curr]) {
+            res+=('a'+curr);
+            alphaT[curr]--;
+        } else {
+            curr++;
+        }
+    }
+    cout<<res<<endl;
 }
 
 int32_t main() {
