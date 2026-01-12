bacd1b690bf4d488c0abe1e15db4d68c3a6064b5
Shridhar Sharma <sridharsharma278@gmail.com>
2026-01-12 17:41:54 +0530

    #121

--- 2026_CP_2026/ACD Ladders GRIND/C_Game_on_Permutation.cpp
+++ 2026_CP_2026/ACD Ladders GRIND/C_Game_on_Permutation.cpp
@@ -9,13 +9,28 @@ inline void fast_io() {
     cin.tie(nullptr);
 }
 
+// Happy New Year EZ
+
 void solve() {
     int n; cin >> n;
     vector<int> permut(n);
     for (int i=0;i<n;i++) {
-        cin >> perumt[i];
+        cin >> permut[i];
     }
-    
+    int loss_min=permut[0], win_min=MOD;
+    int wins=0;
+    for (int i=1;i<n;i++) {
+        if (permut[i]>win_min) {
+            //redundant
+            // loss_min = min(loss_min, permut[0]);
+        } else if (permut[i]>loss_min) {
+            win_min = permut[i];
+            wins++;
+        } else {
+            loss_min = permut[i];
+        }
+    }
+    cout<<wins<<endl;
 }
 
 int32_t main() {
