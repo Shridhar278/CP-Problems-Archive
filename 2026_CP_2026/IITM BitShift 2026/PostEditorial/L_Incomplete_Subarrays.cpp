2052661778db800710700ccfe41f275b648486da
Shridhar Sharma <sridharsharma278@gmail.com>
2026-01-30 15:42:07 +0530

    #142

--- 2026_CP_2026/IITM BitShift 2026/PostEditorial/L_Incomplete_Subarrays.cpp
+++ 2026_CP_2026/IITM BitShift 2026/PostEditorial/L_Incomplete_Subarrays.cpp
@@ -9,14 +9,25 @@ inline void fast_io() {
     cin.tie(nullptr);
 }
 
+// HAVING a GOOD DAY
 // intution : check sub-arrays of length 2&3 only;
 
 void solve() {
     int n, k; cin >> n >> k;
     vector<int> arr(n);
     for (int i=0;i<n;i++) {
-        
+        cin >> arr[i];
     }
+    int best=-1;
+    for (int i=0;i<n-1;i++) {
+        best = max(best, min(arr[i], arr[i+1]));
+    }
+    for (int i=0;i<n-2;i++) {
+        int least = min({arr[i], arr[i+1], arr[i+2]});
+        int limit = arr[i]+arr[i+1]+arr[i+2]-max({arr[i], arr[i+1], arr[i+2]})-least;
+        best = max(best, min(limit, least+k));
+    }
+    cout<<best<<endl;
 }
 
 int32_t main() {
