e41d4f2e4945b847b796ff4562d06e2677335521
Shridhar Sharma <sridharsharma278@gmail.com>
2026-02-19 21:23:14 +0530

    Trademark Template COMPLETE

---
+++ 2026_CP_2026/MidSems aane wale hain/sample.cpp
@@ -0,0 +1,84 @@
+/////////////////////////////////////////////////////////////////////////////
+// @@@@@  @   @  @@@@  @@@@@ @@@@   @   @   @@@   @@@@   @@@@@ @@@@@ @@@@@ //
+// @      @   @  @   @   @   @   @  @   @  @   @  @   @      @    @  @   @ //
+// @@@@@  @@@@@  @@@@    @   @   @  @@@@@  @@@@@  @@@@   @@@@@   @    @@@  //
+//     @  @   @  @  @    @   @   @  @   @  @   @  @  @   @       @   @   @ //
+// @@@@@  @   @  @   @ @@@@@ @@@@   @   @  @   @  @   @  @@@@@   @   @@@@@ //
+/////////////////////////////////////////////////////////////////////////////
+//smudge your eyes a little to see the trademark
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long
+
+void _print(int a) {
+   cout << a;
+}
+void _print(string a) {
+   cout << a;
+}
+void _print(char a) {
+   cout << a;
+}
+template<class T> void _print(vector<T> v) {
+    cout << "[ ";
+    for (auto i : v) {
+        _print(i);
+        cout << " ";
+    }
+    cout << "]";
+}
+template<class T> void _print(map<T, T> v) {
+    cout << "[ ";
+    for (auto [i, j] : v) {
+        cout << "{ ";
+        _print(i);
+        cout << " ";
+        _print(j);
+        cout << " }";
+    }
+    cout << "]";
+}
+
+#define debug(x) \
+   do { \
+        if (check) \
+            cout<<#x<<" = "; _print(x); \
+    } while(0)
+
+#define loop(i, n) for(int i=0;i<n;i++)
+#define precise(x) cout << fixed << setprecision(x)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define map map<int, int>
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define el cout<<endl;
+
+const int mod7 = 1e9 + 7;
+const int mod9 = 998244353;
+const int INF = 1e18;
+const double EPS = 1e-9;
+
+inline void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+#define check 1
+void solve() {
+    vector<int> v = {1, 2, 3, 4, 5};
+    map m = {{1, 2}, {3, 4}, {5, 6}};
+    debug(v);el;
+    debug(m);el;
+}
+
+int32_t main() {
+    fast_io();
+    int test;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
