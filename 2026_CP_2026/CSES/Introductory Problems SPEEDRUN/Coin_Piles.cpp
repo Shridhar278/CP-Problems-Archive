e69db6069abd329cb17b938ec622dd677140cd34
Shridhar Sharma <sridharsharma278@gmail.com>
2026-02-22 12:10:51 +0530

    CSES #20

---
+++ 2026_CP_2026/CSES/Introductory Problems SPEEDRUN/Coin_Piles.cpp
@@ -0,0 +1,89 @@
+/////////////////////////////////////////////////////////////////////////////
+// @@@@@  @   @  @@@@  @@@@@ @@@@   @   @   @@@   @@@@   @@@@@ @@@@@ @@@@@ //
+// @      @   @  @   @   @   @   @  @   @  @   @  @   @      @    @  @   @ //
+// @@@@@  @@@@@  @@@@    @   @   @  @@@@@  @@@@@  @@@@   @@@@@   @    @@@  //
+//     @  @   @  @  @    @   @   @  @   @  @   @  @  @   @       @   @   @ //
+// @@@@@  @   @  @   @ @@@@@ @@@@   @   @  @   @  @   @  @@@@@   @   @@@@@ //
+/////////////////////////////////////////////////////////////////////////////
+//smudge your eyes a little to see the trademark
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long
+
+void _print(int a) {
+   cout << a;
+}
+void _print(string a) {
+   cout << a;
+}
+void _print(char a) {
+   cout << a;
+}
+template<class T> void _print(vector<T> v) {
+    cout << "[ ";
+    for (auto i : v) {
+        _print(i);
+        cout << " ";
+    }
+    cout << "]";
+}
+template<class T> void _print(map<T, T> v) {
+    cout << "[ ";
+    for (auto [i, j] : v) {
+        cout << "{ ";
+        _print(i);
+        cout << " ";
+        _print(j);
+        cout << " }";
+    }
+    cout << "]";
+}
+
+#define debug(x) \
+   do { \
+        if (check) {\
+            cout<<#x<<" = "; _print(x); cout<<endl \
+        } \
+    } while(0)
+
+#define loop(k, n) for(int i=k;i<n;i++)
+#define precise(x) cout << fixed << setprecision(x)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define map map<int, int>
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define el cout<<endl;
+
+const int mod7 = 1e9 + 7;
+const int mod9 = 998244353;
+const int INF = 1e18;
+const double EPS = 1e-9;
+
+inline void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+#define check 1
+void solve() {
+    int a, b; cin >> a >> b;
+    if ((a+b)%3!=0) {
+        no;
+    } else if (max(a, b) > 2*min(a,b)) {
+        no;
+    } else {
+        yes;
+    }
+}
+
+int32_t main() {
+    fast_io();
+    int test;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
