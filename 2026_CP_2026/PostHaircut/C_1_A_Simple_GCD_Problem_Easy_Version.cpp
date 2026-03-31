6a2459a26c0719cd1981be53d75c11758979e3b2
Shridhar Sharma <sridharsharma278@gmail.com>
2026-03-31 20:57:40 +0530

    191

---
+++ 2026_CP_2026/PostHaircut/C_1_A_Simple_GCD_Problem_Easy_Version.cpp
@@ -0,0 +1,101 @@
+/////////////////////////////////////////////////////////////////////////////
+// @@@@@  @   @  @@@@  @@@@@ @@@@   @   @   @@@   @@@@   @@@@@ @@@@@ @@@@@ //
+// @      @   @  @   @   @   @   @  @   @  @   @  @   @      @    @  @   @ //
+// @@@@@  @@@@@  @@@@    @   @   @  @@@@@  @@@@@  @@@@   @@@@@   @    @@@  //
+//     @  @   @  @  @    @   @   @  @   @  @   @  @  @   @       @   @   @ //
+// @@@@@  @   @  @   @ @@@@@ @@@@   @   @  @   @  @   @  @@@@@   @   @@@@@ //
+/////////////////////////////////////////////////////////////////////////////
+//smudge your eyes a little to see the trademark
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long
+
+void _print(int a) {
+   cout << a;
+}
+void _print(string a) {
+   cout << a;
+}
+void _print(char a) {
+   cout << a;
+}
+template<class T> void _print(vector<T> v) {
+    cout << "[ ";
+    for (auto i : v) {
+        _print(i);
+        cout << " ";
+    }
+    cout << "]";
+}
+template<class T> void _print(map<T, T> v) {
+    cout << "[ ";
+    for (auto [i, j] : v) {
+        cout << "{ ";
+        _print(i);
+        cout << " ";
+        _print(j);
+        cout << " }";
+    }
+    cout << "]";
+}
+
+#define debug(x) \
+   do { \
+        if (check) {\
+            cout<<#x<<" = "; _print(x); cout<<endl; \
+        } \
+    } while(0);
+
+#define loop(k, n) for(int i=k;i<n;i++)
+#define precise(x) cout << fixed << setprecision(x)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define map map<int, int>
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define el cout<<endl;
+
+const int mod7 = 1e9 + 7;
+const int mod9 = 998244353;
+const int INF = 1e18;
+const double EPS = 1e-9;
+
+inline void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+#define check 1
+void solve() {
+    int n; cin >> n;
+    vi a(n); loop(0, n) cin >> a[i];
+    vi b(n); loop(0, n) cin >> b[i];
+    vi gcds(n-1);
+    loop(0, n-1) {
+        gcds[i]=__gcd(a[i], a[i+1]);
+    }
+    int count=0;
+    if (a[0]>gcds[0]) {
+        count++;
+    }
+    if (a[n-1]>gcds[n-2]) {
+        count++;
+    }
+    loop(1, n-1) {
+        if (a[i]>gcds[i-1]*gcds[i]/__gcd(gcds[i-1], gcds[i])) {
+            count++;
+        }
+    }
+    cout<<count<<endl;
+}
+
+int32_t main() {
+    fast_io();
+    int test;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
