31d49662201583b4105ec65753181fb35945e817
Shridhar Sharma <sridharsharma278@gmail.com>
2026-04-23 22:37:14 +0530

    #232

---
+++ 2026_CP_2026/POTDs/A_Flip_Flops.cpp
@@ -0,0 +1,93 @@
+/////////////////////////////////////////////////////////////////////////////
+// @@@@@  @   @  @@@@  @@@@@ @@@@   @   @   @@@   @@@@   @@@@@ @@@@@ @@@@@ //
+// @      @   @  @   @   @   @   @  @   @  @   @  @   @      @    @  @   @ //
+// @@@@@  @@@@@  @@@@    @   @   @  @@@@@  @@@@@  @@@@   @@@@@   @    @@@  //
+//     @  @   @  @  @    @   @   @  @   @  @   @  @  @   @       @   @   @ //
+// @@@@@  @   @  @   @ @@@@@ @@@@   @   @  @   @  @   @  @@@@@   @   @@@@@ //
+/////////////////////////////////////////////////////////////////////////////
+//smudge your eyes a little to see the trademark
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int  long long
+
+void _print(int a) {
+   cout << a;
+}
+void _print(string a) {
+   cout << a;
+}
+void _print(char a) {
+   cout << a;
+}
+template<class T> void _print(vector<T> v) {
+    cout << "[ ";
+    for (auto i : v) {
+        _print(i);
+        cout << " ";
+    }
+    cout << "]";
+}
+template<class T> void _print(map<T, T> v) {
+    cout << "[ ";
+    for (auto [i, j] : v) {
+        cout << "{ ";
+        _print(i);
+        cout << " ";
+        _print(j);
+        cout << " }";
+    }
+    cout << "]";
+}
+
+#define debug(x) \
+   do { \
+        if (check) {\
+            cout<<#x<<" = "; _print(x); cout<<endl; \
+        } \
+    } while(0);
+
+#define loop(k, n) for(int i=k;i<n;i++)
+#define precise(x) cout << fixed << setprecision(x)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define map map<int, int>
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define el cout<<endl;
+
+const int mod7 = 1e9 + 7;
+const int mod9 = 998244353;
+const int INF = 1e18;
+const double EPS = 1e-9;
+
+inline void fast_io() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+
+#define check 1
+void solve() {
+    int n, c, k; cin >> n >> c >> k;
+    vi arr(n); loop(0, n) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+    int scope=0;
+    for (int i=0;i<n;i++) {
+        if (c >= arr[i]) {
+            int curr = c - arr[i];
+            c+=min(curr, k-scope)+arr[i];
+            scope = min(scope+curr, k);
+        }
+    }
+    cout<<c<<endl;
+}
+
+int32_t main() {
+    fast_io();
+    int test;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
