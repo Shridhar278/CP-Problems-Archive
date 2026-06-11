f22a5b524a48d2ca45a5eaa7903dda94bedd56a9
Shridhar Sharma <sridharsharma278@gmail.com>
2026-06-11 22:36:21 +0530

    #259

--- 2026_CP_SUMMER_GRIND/Phase Tres THE CONTEST/C_2_Equal_Multisets_Hard_Version.cpp
+++ 2026_CP_SUMMER_GRIND/Phase Tres THE CONTEST/C_2_Equal_Multisets_Hard_Version.cpp
@@ -66,9 +66,138 @@ inline void fast_io() {
     cin.tie(nullptr);
 }
 
-#define check 1
+// LATER ON
+
+// all MATCH condition
+// or CYCLES
+
+// c'mon i got this the last time
+
+
+// BRAIN DAMAGE
+// CP do tmrw NOW
+
+#define check 0
 void solve() {
-    
+    int n, k; cin >> n >> k;
+
+    vi og(n);
+    loop(0, n) {
+        cin >> og[i];
+    }
+
+    vi arr(n);
+    loop(0, n) {
+        cin >> arr[i];
+    }
+
+    vi issues(n, 0);
+    vi locked(n, 0);
+    loop(0, n) {
+        if (arr[i]==og[i] and locked[i%k]!=-1) {
+            locked[i%k]=1;
+        }
+        if (arr[i]!=-1 and arr[i]!=og[i]) {
+            issues[i%k]=1;
+            locked[i%k]=-1;
+        }
+    }
+    loop(0, k) {
+        if (locked[i]==-1) {
+            locked[i]=0;
+        }
+    }
+
+    // all match
+    int flag1=0;
+    loop(0, n) {
+        if (arr[i]==-1) {
+            // okay
+        } else {
+            if (arr[i]!=og[i]) {
+                flag1=1;
+                break;
+            }
+        }
+    }
+    if (!flag1) {
+        yes;
+        return;
+    }
+
+    vi isto1(k, 0);
+    int flag2=0;
+    for (int i=0;i<n;i++) {
+        debug(isto1)
+        if (isto1[i%k]){
+            if (og[i]!=isto1[i%k]) {
+                if (issues[i%k]) {
+                    flag2=1;
+                    break;
+                }
+            }
+        } else {
+            if (!locked[i%k]) {
+                isto1[i%k]=og[i];
+            }
+        }
+    }
+    if (flag2) {
+        no;
+        return;
+    }
+
+    vi isto2(k, 0);
+    int flag3=0;
+    for (int i=0;i<n;i++) {
+        debug(isto2)
+        if (isto2[i%k]) {
+            if (arr[i]!=-1) {
+                if (arr[i]!=isto2[i%k]) {
+                    if (issues[i%k]) {
+                        flag3=1;
+                        break;
+                    }
+                }
+            }
+        } else {
+            if (arr[i]!=-1 and !locked[i%k]) {
+                isto2[i%k]=arr[i];
+            }
+        }
+    }
+    if (flag3) {
+        no;
+        return;
+    }
+
+    // sanitary the CASES are same OVERALL check
+    multiset<int> now;
+    loop(0, k) {
+        if (isto2[i]) {
+            now.insert(isto2[i]);
+        }
+    }
+    loop(0, k) {
+        if (locked[i]) {
+            auto it = now.find(isto2[i]);
+            if (it!=now.end()) {
+                now.erase(it);
+            }
+        }
+    }
+    loop(0, k) {
+        auto it = now.find(isto1[i]);
+        if (it!=now.end()) {
+            now.erase(it);
+        }
+    }
+    if ((int)now.size()) {
+        no;
+        return;
+    }
+
+    yes;
 }
 
 int32_t main() {
